1c59b45c855a2806a1b6d46b6bed0a052b6ac5c3
Osquiris503 <esquiurus13579@gmail.com>
2026-05-10 23:20:51 -0600

    La base del proyecto Spring esta lista

---
+++ issp/src/main/java/mx/unam/aragon/ico/is/issp/IsspApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.is.issp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsspApplication.class, args);
+	}
+
+}
---
+++ issp/src/test/java/mx/unam/aragon/ico/is/issp/IsspApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.is.issp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
